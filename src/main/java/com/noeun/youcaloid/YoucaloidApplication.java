bfb746001c5e15a563445a847377917731689f2b
kwakkyoleen <2007kkl@naver.com>
2023-03-27 13:06:26 +0900

    init

---
+++ src/main/java/com/noeun/youcaloid/YoucaloidApplication.java
@@ -0,0 +1,13 @@
+package com.noeun.youcaloid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoucaloidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoucaloidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noeun/youcaloid/YoucaloidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noeun.youcaloid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoucaloidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
