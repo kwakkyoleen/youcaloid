da8a801ea5cbc8235bb24ff6dbf25e54f1ec8152
kwakkyoleen <2007kkl@naver.com>
2023-06-26 00:59:03 +0900

    Update voice channel auto join

--- src/main/java/com/noeun/youcaloid/bot/BotEventListener.java
+++ src/main/java/com/noeun/youcaloid/bot/BotEventListener.java
@@ -1,9 +1,11 @@
 package com.noeun.youcaloid.bot;
 
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.managers.AudioManager;
 
 public class BotEventListener extends ListenerAdapter{
 
@@ -13,8 +15,17 @@ public class BotEventListener extends ListenerAdapter{
         
         User user = event.getAuthor();
         String message = event.getMessage().getContentRaw();
-        System.out.println(user.getId() + " : "+ message);
+        System.out.println(event.getChannel().getName() + " _ " +user.getId() + " : "+ message);
         event.getChannel().sendMessage("receive");
+
+        AudioChannelUnion connectedChannel = event.getMember().getVoiceState().getChannel();
+        if(connectedChannel != null){
+            AudioManager audioManager = event.getGuild().getAudioManager();
+            if((!audioManager.isConnected()) && event.getChannel().getName().equals("ttsvoice")){
+                audioManager.openAudioConnection(connectedChannel);
+            }
+        }
+
     }
 
     @Override
